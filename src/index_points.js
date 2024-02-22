7e31e3e5abf9b2aa1ce11bbf0db8119861d6592e
GailFoad <gail.foad@thenavpeople.co.uk>
2024-02-22 13:52:15 +0000

    Backup

---
+++ src/index_points.js
@@ -0,0 +1,38 @@
+import { fromEvent } from 'rxjs';
+import { map, filter, take } from 'rxjs/operators';
+
+var button = document.getElementById('myButton');
+
+var clicksMade = fromEvent(button, 'click');
+
+var points = clicksMade.pipe(
+    map(function(e) {
+        return {x: e.clientX, y: e.clientY};
+    })
+);
+
+console.log('points', points);
+
+var count = 0;
+
+var subscription = points.subscribe(
+    function onNext(point) {
+       ++count;
+       alert('Clicked:' + JSON.stringify(point));
+        
+        if (count >= 3) {
+            subscription.unsubscribe();
+        }
+            
+    },
+    function onError(error) {
+        console.log(error);
+    },
+    function onComplete() {
+        console.log('done');
+    });
+
+
+
+
+
