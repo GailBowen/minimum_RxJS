15d0e776662eba2371c8e2ade62470934a791bc3
GailFoad <gail.foad@thenavpeople.co.uk>
2024-02-27 17:43:26 +0000

    Fix it so that you can't drag the widget element out of the parent element.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import { fromEvent } from 'rxjs';
-import { map, switchMap, takeUntil, mergeMapTo } from 'rxjs/operators';
+import { map, switchMap, takeUntil } from 'rxjs/operators';
 
 var parent = document.getElementById('parent');
 var widget = document.getElementById('widget');
+
+var parentPosition = parent.getBoundingClientRect();
+var parentWidth = parentPosition.width;
+var parentHeight = parentPosition.height;
+
+var parentX = parentPosition.x;
+var parentY = parentPosition.y;
+
+var widgetPosition = widget.getBoundingClientRect();
+var widgetWidth = widgetPosition.width;
+var widgetHeight = widgetPosition.height;
+
+var maxWidgetX = parentX + parentWidth - widgetWidth;
+
+var maxWidgetY = parentY + parentHeight - widgetHeight;
 
 var mouseDowns = fromEvent(widget, 'mousedown');
 var parentMouseMoves = fromEvent(parent, 'mousemove');
@@ -13,9 +28,29 @@ var drags = mouseDowns.pipe(
         return parentMouseMoves.pipe(
             map(function(move) {
                 move.preventDefault(); // prevent default behavior of dragging
+
+                var xPosition = move.clientX - start.offsetX;
+                var yPosition = move.clientY - start.offsetY;
+
+                if (xPosition  > maxWidgetX) {
+                    xPosition = maxWidgetX;
+                }
+
+                if (xPosition < parentX) {
+                    xPosition = parentX;
+                }
+
+                if (yPosition > maxWidgetY) {
+                    yPosition = maxWidgetY;
+                }
+
+                if (yPosition < parentY) {
+                    yPosition = parentY;
+                }
+
                 return {
-                    left: move.clientX - start.offsetX,
-                    top: move.clientY - start.offsetY
+                    left: xPosition,
+                    top: yPosition
                 };
             }),
             takeUntil(parentMouseUps)
@@ -25,7 +60,7 @@ var drags = mouseDowns.pipe(
 
 var subscription = drags.subscribe(
     function onNext(position) {
-        console.log('Next:', position);
+        // console.log('Next:', position);
         widget.style.left = position.left + 'px';
         widget.style.top = position.top + 'px';
     },
